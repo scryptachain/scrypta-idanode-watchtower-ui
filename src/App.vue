3785471858f8ccffbf8adcf131cc7027911252fd
turinglabsorg <turinglabs@criptext.com>
2020-05-06 11:14:31 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+      <b-navbar>
+        <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <img src="/logo.png" />
+          </b-navbar-item>
+        </template>
+        <template slot="start">
+          <b-navbar-item href="/#/">IdaNode network</b-navbar-item>
+          <b-navbar-item href="/#/planum">Planum sidechains</b-navbar-item>
+        </template>
+      </b-navbar>
+      <router-view />
+      <hr />Scrypta IdaNode Watchtower
+      <a
+        href="https://github.com/scryptachain/scrypta-idanode-watchtower"
+        target="_blank"
+      >open-source</a> project by
+      <a href="https://scrypta.foundation" target="_blank">Scrypta Foundation</a>.
+      <br />
+      <br />
+  </div>
+</template>
+
+<script>
+  let ScryptaCore = require("@scrypta/core");
+
+  export default {
+    data() {
+      return {
+        scrypta: new ScryptaCore(true)
+      };
+    },
+    methods: {
+    }
+  };
+  </script>
+
+<style>
+  #app {
+    font-family: "Sen";
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="home">
+    <div v-if="!isLoading" style="width:100%; display:block; overflow:hidden;">
+      <div class="card" style="margin:1% 10px; width: 48%; float: left;" v-for="node in sync" v-bind:key="node.url">
+        <div class="card-content">
+          <p class="title">
+            {{ node.url }}
+          </p>
+        </div>
+        <footer class="card-footer">
+          <p class="card-footer-item">
+            <a :href="node.url" target="_blank">
+              <span>
+                View page
+              </span>
+            </a>
+          </p>
+          <p class="card-footer-item">
+            <span v-if="node.response.blocks === undefined">
+              CHECKING
+            </span>
+            <span v-if="node.response.blocks !== undefined">
+              <span v-if="node.response.toindex === 0" style="color: green">
+                SYNCED {{ node.response.blocks }} BLOCKS
+              </span>
+              <span v-if="node.response.toindex > 0" style="color: orange">
+                OUT OF {{ node.response.toindex }} BLOCKS
+              </span>
+            </span>
+          </p>
+        </footer>
+      </div>
+    </div>
+    <div v-if="isLoading" style="text-align:center; padding:25vh 0">
+      <img src="logo.png" width="100"><br><br>
+      Loading IdaNodes Network
+    </div>
+  </div>
+</template>
+
+<script>
+  let ScryptaCore = require("@scrypta/core");
+
+  export default {
+    data() {
+      return {
+        scrypta: new ScryptaCore(true),
+        nodes: [],
+        isLoading: true,
+        sync: {},
+        blocks: {}
+      };
+    },
+    mounted() {
+      const app = this
+      app.nodes = app.scrypta.returnNodes()
+      for(let x in app.nodes){
+        let node = app.nodes[x]
+        app.sync[node] = 'CHECKING'
+      }
+      app.checkNodes()
+      setInterval(function() {
+        app.checkNodes()
+      }, 10000)
+    },
+    methods: {
+      async checkNodes(){
+        const app = this
+        for(let x in app.nodes){
+          let node = app.nodes[x]
+          app.sync[node] = {
+            "url": node,
+            "response": {}
+          }
+          app.$forceUpdate();
+          let response = await app.scrypta.get('/wallet/getinfo', node)
+          if(response.toindex !== undefined){
+            app.sync[node].response = response
+          }
+        }
+        app.$forceUpdate();
+        app.isLoading = false
+      }
+    }
+  };
+</script>
+<style>
+  .home{
+    padding: 20px;
+  }
+  @media screen and (max-width:768px){
+    .title{ font-size: 14px; }
+  }
+</style> 
