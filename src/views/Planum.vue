19405dcf98ba9cc7560cee75d358f3a1accb225f
turinglabsorg <turinglabs@criptext.com>
2020-05-07 15:00:41 +0200

    min fix

--- src/views/Planum.vue
+++ src/views/Planum.vue
@@ -2,11 +2,11 @@
   <div class="home">
     <div v-if="!isLoading" style="width:100%;">
       <div v-for="sidechain in sidechains" v-bind:key="sidechain.address" style="margin-bottom:15px">
-        <div class="card" v-if="sidechain.last_24 > 0">
+        <div class="card">
           <div class="card-content">
             <div class="media">
             <div class="media-left">
-              <figure class="image is-128x128">
+              <figure class="image gravatar is-128x128">
                 <v-gravatar :email="sidechain.address" />
               </figure>
             </div>
@@ -83,64 +83,62 @@
         const app = this
         for(let xxx in app.sidechains){
           let sidechain = app.sidechains[xxx].address
-          if(app.sidechains[xxx].last_24 > 0){
-            // console.log('CHECKING SIDECHAIN ' + sidechain)
-            const responses = {}
-            app.sidechains[xxx].cap = '-'
-            app.sidechains[xxx].burned = '-'
+          // console.log('CHECKING SIDECHAIN ' + sidechain)
+          const responses = {}
+          app.sidechains[xxx].cap = '-'
+          app.sidechains[xxx].burned = '-'
 
-            for(let node in app.nodes){
-              try{
-                // console.log('CHECKING NODE ' + app.nodes[node])
-                app.status[sidechain][app.nodes[node]] = 'CHECKING'
-                app.$forceUpdate();
-                let response = await axios.post(app.nodes[node] + '/sidechain/shares', {sidechain_address: sidechain})
-                responses[app.nodes[node]] = response.data
-                app.status[sidechain][app.nodes[node]] = response.data.cap + ' ' + app.sidechains[xxx].genesis.symbol + ' | ' + response.data.burned + ' ' + app.sidechains[xxx].genesis.symbol
-                app.$forceUpdate();
-              }catch(e){
-                // console.log('NODE ' + app.nodes[node] + ' NOT AVAILABLE')
-              }
+          for(let node in app.nodes){
+            try{
+              // console.log('CHECKING NODE ' + app.nodes[node])
+              app.status[sidechain][app.nodes[node]] = 'CHECKING'
+              app.$forceUpdate();
+              let response = await axios.post(app.nodes[node] + '/sidechain/shares', {sidechain_address: sidechain})
+              responses[app.nodes[node]] = response.data
+              app.status[sidechain][app.nodes[node]] = response.data.cap + ' ' + app.sidechains[xxx].genesis.symbol + ' | ' + response.data.burned + ' ' + app.sidechains[xxx].genesis.symbol
+              app.$forceUpdate();
+            }catch(e){
+              // console.log('NODE ' + app.nodes[node] + ' NOT AVAILABLE')
             }
-            let same = []
-            let burned = []
-            let difference = []
-            for(let x in responses){
-              if(same.length === 0){
-                same.push(responses[x].cap)
-              }else{
-                let lastindex = same.length - 1
-                if(same[lastindex] === responses[x].cap){
-                  same.push(responses[x].cap)
-                  burned.push(responses[x].burned)
-                }else{
-                  difference.push(x)
-                }
-              }
-            }
-            // console.log(same.length + ' NODES ARE EQUALS, TOTAL CAP IS ' + same[0])
-            app.sidechains[xxx].cap = same[0]
-            app.sidechains[xxx].burned = burned[0]
-            let balances = {}
-
-            for(let x in responses){
-              for(let y in responses[x].shares){
-                //// console.log('CHECKING ' + y + ' BALANCE: ' + responses[x].shares[y].balance)
-                if(balances[y] === undefined){
-                  balances[y] = [responses[x].shares[y].balance]
-                }else{
-                  let lastindex =  balances[y].length - 1
-                  if(balances[y][lastindex] === responses[x].shares[y].balance){
-                    balances[y].push(responses[x].shares[y].balance)
-                  }else{
-                    // console.log(y + ' BALANCE IS DIFFERENT ON NODE ' + x)
-                  }
-                }
-              }
-            }
-            global['isChecking'] = false
-            // console.log('CHECKING ENDED')
           }
+          let same = []
+          let burned = []
+          let difference = []
+          for(let x in responses){
+            if(same.length === 0){
+              same.push(responses[x].cap)
+            }else{
+              let lastindex = same.length - 1
+              if(same[lastindex] === responses[x].cap){
+                same.push(responses[x].cap)
+                burned.push(responses[x].burned)
+              }else{
+                difference.push(x)
+              }
+            }
+          }
+          // console.log(same.length + ' NODES ARE EQUALS, TOTAL CAP IS ' + same[0])
+          app.sidechains[xxx].cap = same[0]
+          app.sidechains[xxx].burned = burned[0]
+          let balances = {}
+
+          for(let x in responses){
+            for(let y in responses[x].shares){
+              //// console.log('CHECKING ' + y + ' BALANCE: ' + responses[x].shares[y].balance)
+              if(balances[y] === undefined){
+                balances[y] = [responses[x].shares[y].balance]
+              }else{
+                let lastindex =  balances[y].length - 1
+                if(balances[y][lastindex] === responses[x].shares[y].balance){
+                  balances[y].push(responses[x].shares[y].balance)
+                }else{
+                  // console.log(y + ' BALANCE IS DIFFERENT ON NODE ' + x)
+                }
+              }
+            }
+          }
+          global['isChecking'] = false
+          // console.log('CHECKING ENDED')
         }
       }
     }
@@ -152,5 +150,6 @@
   }
   @media screen and (max-width:768px){
     .title{ font-size: 14px; }
+    .gravatar{display:none}
   }
 </style> 
